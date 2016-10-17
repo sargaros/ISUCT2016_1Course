d974cfc2d03d574406b85149595e3c35305e5700
Gritish765 <kropotina2108@mail.ru>
2016-10-17 11:36:53 +0400

    123

---
+++ src/Phone.java
@@ -0,0 +1,17 @@
+/**
+ * Created by stud_6 on 17.10.16.
+ */
+public class Phone {
+    String brand;
+    String model;
+    private int size;
+
+    public void takeNumber(){
+        System.out.println("Taking a number");
+
+    }
+    void setNumber(){
+        System.out.println("Setting a numbers");
+
+    }
+}
