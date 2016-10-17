844937988789a3f479a35bc0ffa845961c847916
LoSteF1x <crazygpx@mail.ru>
2016-10-17 11:32:51 +0400

    11

---
+++ src/Rabbit.java
@@ -0,0 +1,9 @@
+/**
+ * Created by stud_6 on 17.10.16.
+ */
+public class Rabbit {
+    int Age;
+    String name;
+            String weight;
+    Void run(){System.out.println("I run")}
+}
---
+++ src/RabbitTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by stud_6 on 17.10.16.
+ */
+public class RabbitTest {
+    public static void main(String[] args){
+
+    }
+}
