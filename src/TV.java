f116751a2e13f2e8b1e82a597b0402574d5bc0df
anaitya <an-it-ka@mail.ru>
2016-10-17 11:36:07 +0400

    12

---
+++ src/TV.java
@@ -0,0 +1,16 @@
+/**
+ * Created by stud_6 on 17.10.16.
+ */
+public class TV {
+    String brand;
+    String model;
+    int size;
+
+    void takeNumber(){
+    System.out.println("Taking a number");
+
+    }
+   void instaNumber(){
+       system.out.println("installa number")
+   }
+}
