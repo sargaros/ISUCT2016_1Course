50fc4bb17d816ca751c780dc50cbb0b415d8a79b
BPavelS <asdasddddqwerty@gmail.com>
2016-10-17 11:45:31 +0400

    1

---
+++ src/Computer.java
@@ -0,0 +1,28 @@
+/**
+ * Created by stud_6 on 17.10.16.
+ */
+public class Computer {
+    String brand;
+    String model;
+    int volume;
+
+    void volume() {
+        System.out.println(volume);
+    }
+
+    void color() {
+        System.out.println("get color");
+    }
+
+    void weight() {
+        System.out.println("weight colour");
+    }
+}
+
+
+
+
+
+
+
+
---
+++ src/Computerr.java
@@ -0,0 +1,20 @@
+/**
+ * Created by stud_6 on 17.10.16.
+ */
+public class Computerr {
+
+    public static void main(String[] args) {
+        Computer com = new Computer();
+        com.brand = "HDD";
+        com.model = "6200";
+        com.volume = 100000;
+        com.volume();
+    }
+}
+
+
+
+
+
+
+
