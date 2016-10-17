4f8436beb771fedf763d6c6497ca35c7ec0bab4a
BPavelS <asdasddddqwerty@gmail.com>
2016-10-17 11:30:18 +0400

    cat

---
+++ src/Cat.java
@@ -0,0 +1,20 @@
+/**
+ * Created by stud_6 on 17.10.16.
+ */
+public class Cat {
+    int Age;
+    String name;
+    String color;
+
+    void Talk() {
+        System.out.println("May");
+    }
+
+    void Go() {
+        System.out.println("Prisla");
+    }
+
+    void V() {
+        System.out.print(Age);
+    }
+}
---
+++ src/CatTest.java
@@ -0,0 +1,16 @@
+import sun.security.action.LoadLibraryAction;
+
+/**
+ * Created by stud_6 on 17.10.16.
+ */
+public class CatTest {
+    public static void main(String[] args) {
+        Cat c = new Cat();
+        c.Age = 5;
+        c.name = "los";
+        c.color = "black";
+        c.V();
+
+
+    }
+}
