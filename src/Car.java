2808161984022b20e17aeb1c0343301f616f7b4d
Ekaterina <ekaterina_makhalk@mail.ru>
2016-10-17 11:14:51 +0400

    Merge branch 'Ipatov_MA' of https://github.com/ISUCT/ISUCT2016_1Course into Ipatov_MA
    # Conflicts:
    #	Demo.java
    #	README.md

---
+++ src/Car.java
@@ -0,0 +1,20 @@
+/**
+ * Created by stud_6 on 17.10.16.
+ */
+public class Car {
+    String brand;
+    String model;
+    int horsepower;
+
+    void Starter(){
+        System.out.println("Engine start");
+    }
+
+    void CheckOil(){
+        System.out.println("Oil checked");
+    }
+
+    void CheckPressure(){
+        System.out.println("Pressure checked");
+    }
+}
--- src/Demo.java
+++ src/Demo.java
@@ -1,7 +1,29 @@
 public class Demo {
-    public static void main (String[] args){
-	System.out.println("Ipatov Maxim");    
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.brand = "Volkswagen";
+        c.model = "Golf VII";
+        c.horsepower = 120;
+        c.Starter();
+        c.CheckOil();
+        c.CheckPressure();
     }
 }
+
+
+
+
+
+
+
+
+
+/*
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Ipatov Maxim");
+    }
+}
+*/
 
 
