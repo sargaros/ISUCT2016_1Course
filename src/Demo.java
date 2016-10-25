3b6b50829156e7dac988a6dd5488c3e1d04589e8
anaitya <an-it-ka@mail.ru>
2016-10-25 09:41:27 +0400

    12

--- src/Demo.java
+++ src/Demo.java
@@ -2,6 +2,18 @@ public class Demo {
     public static void main(String[] args) {
 
         System.out.println("Manukyan_A_O");
+        TV[] TVs = new TV[5];
+        TVs[0]  = new TV();
+        TVs[0].brand = "LG";
+        TVs[0].takeNumber();
+        TVs[1].brand = "Samsung";
+        TVs[2] = new TVs();
+        TVs[2].brand = "HTC"
+
+                (int a=0; i<5; i++)
+            TVnames[i] = ""
+        Tv[1] = 3; //
+        Tv[2] = 1; //
 
     }
 }
