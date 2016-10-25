8e7c829edfe357dda95fb73846f3aa8a44b3497e
LoSteF1x <crazygpx@mail.ru>
2016-10-25 09:34:35 +0400

    11

--- src/RabbitTest.java
+++ src/RabbitTest.java
@@ -3,6 +3,14 @@
  */
 public class RabbitTest {
     public static void main(String[] args){
-
+Rabbit cam = new Rabbit();
+        Rabbit c = new Rabbit();
+        Rabbit[] pub;
+        pub = new Rabbit [3];
+        pub[0] =  new Rabbit();
+        pub[1] = new Rabbit();
+        pub[2] = new Rabbit();
+        pub [1].Age = 5;
+        System.out.print(pub[1].Age);
     }
 }
