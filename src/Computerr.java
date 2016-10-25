dc2dc5c724d03c35507ca4f71e9884190b9922dd
BPavelS <asdasddddqwerty@gmail.com>
2016-10-25 09:33:53 +0400

    1

--- src/Computerr.java
+++ src/Computerr.java
@@ -8,7 +8,13 @@ public class Computerr {
         com.brand = "HDD";
         com.model = "6200";
         com.volume = 100000;
-        com.volume();
+        Computer[] mainchick ;
+        mainchick = new Computer[2];
+        mainchick[0]=new Computer();
+        mainchick[1]=new Computer();
+        mainchick[0].volume=6200;
+        mainchick[1].volume=6300;
+    System.out.println(mainchick[1].volume);
     }
 }
 
