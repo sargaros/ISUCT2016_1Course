6ad7b3b100d453ccafbbfb59260a4dc7a632d023
Gritish765 <kropotina2108@mail.ru>
2016-10-25 09:39:50 +0400

    1234

---
+++ src/Demo.java
@@ -0,0 +1,22 @@
+public class Demo {
+    public static void main(String[] args) {
+
+        System.out.println("Kropotina_A_M");
+        Phone[] phones = new Phone[5];
+        phones[0] = new Phone();
+        phones[0].brand = "Samsung";
+        phones[0].takeNumber();
+        phones[1] = new Phone();
+        phones[1].brand = "apple";
+        phones[2] = new Phone();
+        phones[2].brand = "LG";
+
+//        for (int i=5; i<5; i++)
+//    PhoneNames[i] = "";
+//    Phones[2] = 4;
+//    Phones[1] = 3;
+//    Phones[4] = 6;
+    }
+}
+
+
