69f96a23c87de118f4edfad64c9198bd2c2f6dde
alexanderyershov1 <alexanderyershov1@gmail.com>
2016-10-31 11:45:57 +0400

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,16 @@
+public class Dog {
+    String name = "Spotti";
+    String color = "White":
+    String age = "1 year";
+    String weight = "10 kg";
+    void Talk() {
+        System.out.println("Gav-gav!");
+    }
+
+    void Go(){
+        System.out.println("Bezhit");
+    }
+    void getInformation(){
+        System.out.printn(age);
+    }
+}
