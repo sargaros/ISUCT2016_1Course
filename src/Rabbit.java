5339d558db0f4d58b14c4e23e0815d9bc7515db7
LoSteF1x <crazygpx@mail.ru>
2016-10-31 11:19:31 +0400

    11

--- src/Rabbit.java
+++ src/Rabbit.java
@@ -2,8 +2,36 @@
  * Created by stud_6 on 17.10.16.
  */
 public class Rabbit {
-    int Age;
-    String name;
-            String weight;
-    Void run(){System.out.println("I run")}
+    private int Age;
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    private String name;
+  private String weight;
+
+    void run() {
+        System.out.println("I run");
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
 }
--- src/RabbitTest.java
+++ src/RabbitTest.java
@@ -2,15 +2,18 @@
  * Created by stud_6 on 17.10.16.
  */
 public class RabbitTest {
-    public static void main(String[] args){
-Rabbit cam = new Rabbit();
+    public static void main(String[] args) {
+        Rabbit cam = new Rabbit();
         Rabbit c = new Rabbit();
         Rabbit[] pub;
-        pub = new Rabbit [3];
-        pub[0] =  new Rabbit();
+        pub = new Rabbit[3];
+        pub[0] = new Rabbit();
         pub[1] = new Rabbit();
         pub[2] = new Rabbit();
-        pub [1].Age = 5;
-        System.out.print(pub[1].Age);
+        pub[1].setAge(5);
+        System.out.print(pub[1].getAge());
     }
 }
+
+
+
