45303f26434ce76290515493d101505e40a4f8a5
LoSteF1x <crazygpx@mail.ru>
2016-11-08 09:29:48 +0400

    Test228

---
+++ test/RabbitTestTest.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by stud_6 on 08.11.16.
+ */
+public class RabbitTestTest {
+    @Test
+    public void summ() throws Exception {
+
+    }
+
+    @Test
+    public void summ1() throws Exception {
+        RabbitTest rabbit = new RabbitTest();
+        int result = rabbit.summ(2,2);
+        int expResult = 4;
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testSummOtr() throws Exception {
+        RabbitTest rabbit = new RabbitTest();
+        int result = rabbit.summ(-2, -2);
+        int expResult = -4;
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testSummDiffZeroRes() throws Exception {
+        RabbitTest rabbit = new RabbitTest ();
+        int result = rabbit.summ(-2, 2);
+        int expResult = 0;
+//        assertEquals(expResult, result);
+    }
+    @Test
+    public void testSummDiffNonZeroRes() throws Exception {
+        RabbitTest rabbit = new RabbitTest();
+        int result = rabbit.summ(-2, 3);
+        int expResult = 1;
+//        assertEquals(expResult, result);
+    }
+}
