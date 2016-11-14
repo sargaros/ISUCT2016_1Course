17a8a9eb4016aca0b3f042be93033452234936d3
Maxim Ipatov <stud_6@6_07.efdnew.isuct.ru>
2016-11-14 11:24:51 +0400

    Test DotCom, DotCom

---
+++ src/FirstGame/DotCom.java
@@ -0,0 +1,7 @@
+package FirstGame;
+
+/**
+ * Created by stud_6 on 14.11.16.
+ */
+public class DotCom {
+}
---
+++ test/FirstGame/DotComTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by stud_6 on 14.11.16.
+ */
+public class DotComTest {
+
+}
