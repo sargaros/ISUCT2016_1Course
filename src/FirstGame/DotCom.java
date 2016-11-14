21ef7f9aaed2998bd06e2bd8c066e7aed4e24246
Maxim Ipatov <stud_6@6_07.efdnew.isuct.ru>
2016-11-14 11:30:43 +0400

    Test DotCom, DotCom v1.1

--- src/FirstGame/DotCom.java
+++ src/FirstGame/DotCom.java
@@ -3,5 +3,30 @@ package FirstGame;
 /**
  * Created by stud_6 on 14.11.16.
  */
+import java.util.ArrayList;
 public class DotCom {
+    private ArrayList<String> locationCells;
+    // private int numOfHits
+    // сейчас нам это не нужно
+
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
+
 }
+    public String checkYourself(String userInput) {
+        String result = "Мимо";
+            int index = locationCells.indexOf(userInput);
+            if (index >= 0) {
+                locationCells.remove(index);
+
+                if (locationCells.isEmpty()) {
+                    result = "Потопил";
+                } else {
+                result = "Попал";
+                } //Конец if
+            } // Конец внешнего if
+
+        return result;
+    } // Конец метода
+} // Конец класса
+
--- test/FirstGame/DotComTest.java
+++ test/FirstGame/DotComTest.java
@@ -1,8 +1,111 @@
+package FirstGame;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by stud_6 on 14.11.16.
  */
 public class DotComTest {
+    @Test
+    public void checkYourself1() throws Exception {
+        ArrayList<String> trueLocation = new ArrayList<String>();
+        trueLocation.add("2");
+        trueLocation.add("3");
+        trueLocation.add("4");
+
+        DotCom dotCom = new DotCom();
+        String userGuess = "2";
+        dotCom.setLocationCells(trueLocation);
+        String result = dotCom.checkYourself(userGuess);
+        String expResult = "Попал";
+        assertEquals(expResult, result);
+
+    }
+
+
+    @Test
+    public void checkYourself2() throws Exception {
+        ArrayList<String> trueLocation = new ArrayList<String>();
+        trueLocation.add("2");
+        trueLocation.add("3");
+        trueLocation.add("4");
+
+        DotCom dotCom = new DotCom();
+        String userGuess = "1";
+        dotCom.setLocationCells(trueLocation);
+        String result = dotCom.checkYourself(userGuess);
+        String expResult = "Мимо";
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void checkYourself3() throws Exception
+
+    {
+        ArrayList<String> trueLocation = new ArrayList<String>();
+        trueLocation.add("2");
+        trueLocation.add("3");
+        trueLocation.add("4");
+
+        DotCom dotCom = new DotCom();
+        dotCom.setLocationCells(trueLocation);
+        String result = dotCom.checkYourself("2");
+        assertEquals("Попал", result);
+        result = dotCom.checkYourself("3");
+        assertEquals("Попал", result);
+        result = dotCom.checkYourself("1");
+        String expResult = "Мимо";
+        assertEquals(expResult, result);
+        result = dotCom.checkYourself("4");
+        assertEquals("Потопил", result);
+
+    }
+
+
+    @Test
+    public void checkYourself4() throws Exception
+
+    {
+        ArrayList<String> trueLocation = new ArrayList<String>();
+        trueLocation.add("2");
+        trueLocation.add("3");
+        trueLocation.add("4");
+
+        DotCom dotCom = new DotCom();
+        dotCom.setLocationCells(trueLocation);
+        String result = dotCom.checkYourself("2");
+        assertEquals("Попал", result);
+        result = dotCom.checkYourself("3");
+        assertEquals("Попал", result);
+        result = dotCom.checkYourself("4");
+        String expResult = "Потопил";
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void checkYourself5() throws Exception {
+
+        ArrayList<String> trueLocation = new ArrayList<String>();
+        trueLocation.add("2");
+        trueLocation.add("2");
+        trueLocation.add("2");
+
+        DotCom dotCom = new DotCom();
+        dotCom.setLocationCells(trueLocation);
+        String result = dotCom.checkYourself("2");
+        assertEquals("Попал", result);
+        result = dotCom.checkYourself("3");
+        assertEquals("Мимо", result);
+        result = dotCom.checkYourself("4");
+        assertEquals("Мимо", result);
+
+    }
+
 
 }
